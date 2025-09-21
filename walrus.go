9d399df3b2693c30f1a149c42e134b7280982543
Besnik Konjuhaj <besnik.konjuhaj@gmail.com>
2025-09-21 23:21:05 +0300

    Try out walrus operator

---
+++ walrus.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	mySkillIssue := 42
+	pi := 3.14159
+	message := "hello world"
+	isGoat := true
+
+	fmt.Println("%v, %v, %v, %v", mySkillIssue, pi, message, isGoat)
+
+}
