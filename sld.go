75a9af08dd383436b7c6533dcdbb93caab19dbaa
Besnik Konjuhaj <besnik.konjuhaj@gmail.com>
2025-09-22 23:17:59 +0300

    Testing limitations of assignment mismatches

--- sld.go
+++ sld.go
@@ -13,4 +13,11 @@ func main() {
 
 	s, b, f := "a string", true, 0.21
 	fmt.Println(s, b, f)	
+
+	// Three variables, two assignments
+	//a, b := 1, 2, 3
+	//fmt.Println(a, b, c)
+	// ERROR MESSAGE: ./sld.go:18:13: assignment mismatch: 3 variables but 2 values
+
+	
 }
