b99b945423dedba4620d5c88a586d45adcb4fe43
Besnik Konjuhaj <besnik.konjuhaj@gmail.com>
2025-09-23 20:18:54 +0300

    Constants with computed time

--- const.go
+++ const.go
@@ -1,6 +1,7 @@
 package main
 
 import "fmt"
+import "time"
 
 func main() {
 
@@ -8,8 +9,12 @@ func main() {
 	const secondsInAMinute = 60
 	const secondsInAnHour = 60*60 // Constants can be a computed value but must be known at compile time
 
-	mutableSecondsInAnHour = 60*60
+	mutableSecondsInAnHour := 60*60 // Var with walrus operator and computed value
 
 	fmt.Println("Number of seconds in an hour:", mutableSecondsInAnHour)
 
+	// Does not work due to time being calculated at run time
+	// const currentTime = time.Now() 
+	currentTime := time.Now()
+	fmt.Println(currentTime)
 }
