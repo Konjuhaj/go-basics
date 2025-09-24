ac03ff298c318a2c68c114a7b3274b27e4ebb13c
Besnik Konjuhaj <besnik.konjuhaj@gmail.com>
2025-09-24 23:14:04 +0300

    Testing if conditions

---
+++ conditions.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	amountOfPeople := 2221
+	const venueLimit int = 30
+
+	if amountOfPeople <= venueLimit {
+		fmt.Println("Everyone got in")
+	} else {
+		fmt.Printf("21 people got in and %v were left out \n",  amountOfPeople - venueLimit)
+	}
+	if outsiders := amountOfPeople - venueLimit; outsiders > 0 {
+		fmt.Printf("%d people went home and are sad\n", outsiders)	
+	}
+
+}
