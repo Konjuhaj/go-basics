9965aa6c4eb507daa1919c29b544c49f810130d8
Besnik Konjuhaj <besnik.konjuhaj@gmail.com>
2025-12-28 02:17:19 +0200

    Test out struct binding

--- types.go
+++ types.go
@@ -12,9 +12,14 @@ func updateUserId(u *User) {
 	fmt.Printf("userId in function is %d \n", u.userId)
 }
 
+func (u User) introduce(greeting string) {
+	fmt.Printf("%s %s", greeting, u.username)
+} 
 
 func main() {
 	user := User{"besnik", 123}
+	defer user.introduce("Hello my username is")	
+
 	updateUserId(&user)
 	fmt.Printf("%s\n", user.username)
 	fmt.Printf("UserID in main is %d\n", user.userId)
